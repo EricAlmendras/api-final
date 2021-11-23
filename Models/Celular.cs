9e0cc7f2771958c43f440054e95effd9ea61c6c9
Eric Almendras <ericalmendras@hotmail.com>
2021-11-23 18:12:42 -0300

    init

---
+++ Models/Celular.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Smartphone.Models
+{
+    public class Celular
+    {
+        public int CelularId { get; set; }
+        public string Nombre { get; set; }
+        public float Precio { get; set; }
+    }
+}
---
+++ Models/SmartphoneContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Smartphone.Models;
+
+namespace Smartphone.Models
+{
+    public class SmartphoneContext : DbContext
+    {
+        public SmartphoneContext(DbContextOptions<SmartphoneContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        public DbSet<Smartphone.Models.Celular> Celular { get; set; }
+ 
+    }
+}
